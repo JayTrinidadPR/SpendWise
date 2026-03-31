6d7967567b73a64772acb7dce04d0f4224ac2fa0
jaytrinidadpr <justinquelix@icloud.com>
2026-03-30 23:39:32 -0500

    fix schema, error seeding password instead of email-seed.js

--- db/schema.js
+++ db/schema.js
@@ -6,7 +6,7 @@ async function createTables() {
             CREATE TABLE IF NOT EXISTS users (
                 id SERIAL PRIMARY KEY,
                 username VARCHAR(50) UNIQUE NOT NULL,
-                password VARCHAR(100) UNIQUE NOT NULL
+                email VARCHAR(100) UNIQUE NOT NULL
             );
         `);
 
